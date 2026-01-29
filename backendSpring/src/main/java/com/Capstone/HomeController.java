7bf4a2e746e9025fc79f058753e8cb6d02fe8f08
LalSang <thomassang07@gmail.com>
2026-01-28 22:57:56 -0500

    backend

---
+++ backendSpring/src/main/java/com/Capstone/HomeController.java
@@ -0,0 +1,17 @@
+package com.Capstone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "StudyBuddyHP";
+    }
+}
